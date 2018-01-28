fde4f7e0af40a1492a473d520f3e1ce193306814
Jelle Grammens <jgrammens@crunchanalytics.be>
2018-01-28 19:44:52 +0100

    major update - last part

--- src/rtcw_functions.py
+++ src/rtcw_functions.py
@@ -3,6 +3,8 @@ import hashlib
 import pandas as pd
 import numpy as np 
 from lxml import etree
+from goldfinch import validFileName
+from lxml import etree
 
 ##########################
 # FUNCTIONS TO PARSE DEMOS
@@ -224,6 +226,12 @@ def get_sec(time_str):
     m, s = time_str.split(':')
     return int(m) * 60 + int(s)
 
+def convert_names(x):
+    '''helper function to create valid player names for files later'''
+    x = str(x)
+    x = validFileName(x)
+    return x
+
 
 def get_kill_sprees(df, maxtime_secs = 30, weapon_filter = None, minspree = 3, verbose = True):
     '''
@@ -258,8 +266,10 @@ def get_kill_sprees(df, maxtime_secs = 30, weapon_filter = None, minspree = 3, v
     pd_attacker = []
     pd_start_dwtime = []
     pd_end_dwtime = []
+    pd_weapons = []
     pd_spree_length = []
     pd_demo_name = []
+    pd_match_name = []
     pd_player_name = []
 
     #filter the dataframe if we have a weapon filter
@@ -277,16 +287,18 @@ def get_kill_sprees(df, maxtime_secs = 30, weapon_filter = None, minspree = 3, v
     for demo in df.szMd5.unique():
         df_demo = df.loc[df['szMd5'] == demo]
         demo_name = df_demo.demoName.unique()
+        match_name = df_demo.matchName.unique()
         counter += 1
         
         for player in df_demo.bAttacker.unique():
             df_cut = df_demo.loc[df['bAttacker'] == player]
             player_name = df_cut.szCleanName.unique()
-            arr = df_cut.as_matrix(columns = ['bAttacker', 'bTarget', 'bIsTeamkill', 'dwTime'])
+            arr = df_cut.as_matrix(columns = ['bAttacker', 'bTarget', 'bIsTeamkill', 'dwTime', 'bWeapon'])
 
             timerestriction = maxtime_secs * 1000 #put it in seconds for rtcw time
             spreecounter = 0
             temp_sprees = []
+            weapons_used = ''
             total_rows = len(arr)
 
             for row in range(len(arr)):
@@ -302,17 +314,21 @@ def get_kill_sprees(df, maxtime_secs = 30, weapon_filter = None, minspree = 3, v
                         spreecounter = 1
                         first_spreetime = frag[3]   
                         temp_sprees = []
-                        temp_sprees.append(first_spreetime)       
+                        weapons_used = ''
+                        temp_sprees.append(first_spreetime)
+                        weapons_used += str(frag[4]) + '-'
                     else:
                         spreecounter = 0
                         first_spreetime = frag[3]   
                         temp_sprees = []
+                        weapons_used = ''
 
                 #if current frag is in the timerestriction, up the spreecount and append temp_sprees
                 if frag[3] - first_spreetime <= timerestriction:
                     if (frag[0] != frag[1]) and (frag[2] != 1):
                         spreecounter += 1
                         temp_sprees.append(frag[3])
+                        weapons_used += str(frag[4]) + '-'
 
                 #if not, add a value to every list with spree details if minspree is met / reinitilize spreecounter and temp_sprees
                 else:
@@ -321,20 +337,25 @@ def get_kill_sprees(df, maxtime_secs = 30, weapon_filter = None, minspree = 3, v
                         pd_attacker.append(player)
                         pd_start_dwtime.append(temp_sprees[0])
                         pd_end_dwtime.append(temp_sprees[-1])
+                        pd_weapons.append(weapons_used)
                         pd_spree_length.append(spreecounter)
                         pd_demo_name.append(demo_name[0])
+                        pd_match_name.append(match_name[0])
                         pd_player_name.append(player_name[0])
 
                     if (frag[0] != frag[1]) and (frag[2] != 1):
                         spreecounter = 1
                         first_spreetime = frag[3]   
                         temp_sprees = []
+                        weapons_used = ''
                         temp_sprees.append(first_spreetime)
+                        weapons_used += str(frag[4]) + '-'
 
                     else:
                         spreecounter = 0
                         first_spreetime = frag[3]   
                         temp_sprees = []
+                        weapons_used = ''
 
                 #if last frag in the data, and we have reach the minspree, add entry
                 if row == (total_rows - 1):
@@ -343,8 +364,10 @@ def get_kill_sprees(df, maxtime_secs = 30, weapon_filter = None, minspree = 3, v
                         pd_attacker.append(player)
                         pd_start_dwtime.append(temp_sprees[0])
                         pd_end_dwtime.append(temp_sprees[-1])
+                        pd_weapons.append(weapons_used)
                         pd_spree_length.append(spreecounter)
                         pd_demo_name.append(demo_name[0])
+                        pd_match_name.append(match_name[0])
                         pd_player_name.append(player_name[0])
 
         if counter % 100 == 0:
@@ -360,10 +383,19 @@ def get_kill_sprees(df, maxtime_secs = 30, weapon_filter = None, minspree = 3, v
      'attacker': pd_attacker,
      'start': pd_start_dwtime,
      'end': pd_end_dwtime,
+     'weapons' : pd_weapons,
      'spreecount': pd_spree_length,
      'demo': pd_demo_name,
+     'match': pd_match_name,
      'player': pd_player_name
     })
+
+    #convert player names to valid names for windows filenaming
+    df_spree['player'] = df_spree.player.apply(lambda x: convert_names(x))
+
+    #make sure a buggy demo is not in there
+    df_spree = df_spree.loc[df_spree['match'] != 'rtcw_2003.06.25_qcon03-qual_gmpo_vs_clan-carnage_round2'].copy()
+    df_spree.reset_index(drop=True, inplace=True)
 
     return df_spree
 
@@ -379,7 +411,20 @@ def locate_demo_path(demos_dct, spree, root_path):
             
             return match_folder, demo_name
 
-def cutter_exe_cmd(root_path, match_folder, demo_name, start_time, end_time, 
+
+def generate_output_name(spree, transform_to_dm_60):
+    #output_name = spree['match'] + '_' +  spree['demo'][:-6] + '_' + spree['player'] + '_' + spree['weapons'] + str(spree['start']) + spree['demo'][-6:]
+    output_name = spree['demo'][:-6] + '_' + spree['player'] + '_' + spree['weapons'] + str(spree['start']) + spree['demo'][-6:]
+    
+    if transform_to_dm_60:
+        output_name = output_name[:-2] + '60'
+
+    #additional replacement 
+    output_name = output_name.replace('^', '')
+    
+    return output_name
+
+def cutter_exe_cmd(root_path, match_folder, demo_name, spree, start_time, end_time, transform_to_dm_60 = True, 
                    demo_folder_name = 'demos', output_folder = 'output_spree_demos', cut_type = 1):
     '''
     helper function to create string with demo_path and parameters to input in anders libtech 3 api
@@ -388,7 +433,9 @@ def cutter_exe_cmd(root_path, match_folder, demo_name, start_time, end_time,
     - parameters_dct: a dictionary with all the parameters necessary
     '''
     demo_path = os.path.join(root_path, demo_folder_name, match_folder, demo_name)
-    output_path = os.path.join(root_path, output_folder, demo_name)
+    output_demo_name = generate_output_name(spree, transform_to_dm_60)
+
+    output_path = os.path.join(root_path, output_folder, output_demo_name)
     
     s = 'cut ' + demo_path + ' '
     s += output_path + ' '
@@ -398,11 +445,10 @@ def cutter_exe_cmd(root_path, match_folder, demo_name, start_time, end_time,
 
     return s
 
-def cut_demos(root_path, demos_dct, df_spree, offset_start = 5, offset_end = 5, 
+def cut_demos(root_path, demos_dct, df_spree, offset_start = 5, offset_end = 5, transform_to_dm_60 = True,
     demo_folder_name = 'demos', output_folder = 'output_spree_demos', exe_name = 'Anders.Gaming.LibTech3.exe', cut_type = 1):
 
     for row in range(len(df_spree)):
-
         spree = df_spree.loc[row]
         exe_path = os.path.join(root_path, exe_name)
         match_folder, demo_name = locate_demo_path(demos_dct, spree, root_path)
@@ -410,8 +456,52 @@ def cut_demos(root_path, demos_dct, df_spree, offset_start = 5, offset_end = 5,
         start_time = spree.start - (offset_start * 1000)
         end_time = spree.end + (offset_end * 1000)
 
-        parameters = cutter_exe_cmd(root_path, match_folder, demo_name, start_time, end_time, 
+        parameters = cutter_exe_cmd(root_path, match_folder, demo_name, spree, start_time, end_time, transform_to_dm_60 = transform_to_dm_60,
                                     demo_folder_name = demo_folder_name, output_folder = output_folder, cut_type = cut_type)
 
         os.system(exe_path + ' ' + parameters)
 
+def generate_capture_list(df_spree, transform_to_dm_60 = True):
+    '''
+    Function that makes a xml capture list to be imported in crumb's his demoviewer
+    '''
+    
+    df_spree['output_name'] = df_spree.apply(lambda x: generate_output_name(x, transform_to_dm_60), axis=1).values
+    
+    captureList = etree.Element('CaptureList')
+
+    for row in range(len(df_spree)):
+        spree = df_spree.loc[row]
+
+        capture = etree.Element('capture')
+        id = etree.Element('id')
+        demoPath = etree.Element('demoPath')
+        startFrame = etree.Element('startFrame')
+        stopFrame = etree.Element('stopFrame')
+        localOffset = etree.Element('localOffset')
+        selected = etree.Element('selected')
+        playerPov = etree.Element('playerPov')
+        config = etree.Element('config')
+
+        id.text = '-1'
+        selected.text = 'No'
+        localOffset.text = '0'
+        demoPath.text = spree.output_name
+        startFrame.text = str(spree.start - 50000)
+        stopFrame.text = str(spree.end + 50000)
+        playerPov.text = str(spree.attacker)
+        config.text = 'follow' + str(spree.attacker) + '.cfg'
+
+        capture.append(id)
+        capture.append(demoPath)
+        capture.append(startFrame)
+        capture.append(stopFrame)
+        capture.append(localOffset)
+        capture.append(selected)
+        capture.append(playerPov)
+        capture.append(config)
+
+        captureList.append(capture)
+        
+    tree = etree.ElementTree(captureList)
+    tree.write('C:\Users\JelleGrammens\Documents\capture_list.xml', pretty_print=True, xml_declaration=True, encoding="utf-8")
